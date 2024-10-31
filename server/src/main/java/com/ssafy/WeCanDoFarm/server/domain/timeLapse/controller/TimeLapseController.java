145e3e829115a520ccc5825f44b938f72bbd7365
ParkBeomSu <beomsu4639@gmail.com>
2024-10-31 17:23:16 +0900

    [FEAT] 중간 저장

---
+++ server/src/main/java/com/ssafy/WeCanDoFarm/server/domain/timeLapse/controller/TimeLapseController.java
@@ -0,0 +1,16 @@
+package com.ssafy.WeCanDoFarm.server.domain.timeLapse.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/api/v1/timeLapse")
+@RequiredArgsConstructor
+public class TimeLapseController {
+
+
+
+}
